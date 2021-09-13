5b9b06eb2b856e4827e626db8103ddabbf6160f4
Jakub Śpiewak <priv@jakubspiewak.com>
2021-09-13 23:42:42 +0200

    sync

--- src/main/java/com/jakubspiewak/ashfileservice/service/FileServiceImpl.java
+++ src/main/java/com/jakubspiewak/ashfileservice/service/FileServiceImpl.java
@@ -11,22 +11,30 @@ import java.util.UUID;
 @RequiredArgsConstructor
 class FileServiceImpl implements FileService {
 
-    private final FileRepository fileRepository;
-    private final FileMapper fileMapper;
+  private final FileRepository fileRepository;
 
-    @Override
-    public UUID save(final ApiFileCreateRequest request) {
-        final var entity = fileMapper.fromRequestToEntity(request);
-        return fileRepository.save(entity).getId();
-    }
+  @Override
+  public UUID save(final ApiFileCreateRequest request) {
+    final var entity = fromRequestToEntity(request);
+    return fileRepository.save(entity).getId();
+  }
 
-    @Override
-    public ApiFileGetResponse get(final UUID id) {
-        return fileRepository.findById(id).map(fileMapper::fromEntityToResponse).orElseThrow();
-    }
+  @Override
+  public ApiFileGetResponse get(final UUID id) {
+    return fileRepository.findById(id).map(this::fromEntityToResponse).orElseThrow();
+  }
 
-    @Override
-    public void delete(final UUID id) {
-        fileRepository.deleteById(id);
-    }
+  @Override
+  public void delete(final UUID id) {
+    fileRepository.deleteById(id);
+  }
+
+  private ApiFileGetResponse fromEntityToResponse(FileEntity entity) {
+    return ApiFileGetResponse.builder().file(entity.getBytes()).fileName(entity.getName()).build();
+  }
+
+  private FileEntity fromRequestToEntity(ApiFileCreateRequest request) {
+    return FileEntity.builder().bytes(request.getFile()).name(request.getFileName()).build();
+  }
+
 }
